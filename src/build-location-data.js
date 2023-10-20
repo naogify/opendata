c0d7f4d47e3b9ae51a039552cf88ce77165178c7
Naoki Ohashi <n.globe.us@gmail.com>
2023-10-20 17:09:11 +0900

    Update build-location-data.js

--- src/build-location-data.js
+++ src/build-location-data.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const { parse } = require('csv-parse');
+const { parse } = require('csv-parse/sync');
 const glob = require('glob');
 const path = require('path');
 const csv2geojson = require('csv2geojson');
@@ -22,7 +22,22 @@ for (let i = 0; i < categories.length; i++) {
 
       const csvString = fs.readFileSync(file, 'utf8');
 
-      csv2geojson.csv2geojson(csvString, function(err, data) {
+      // 緯度経度のフィールド名を判定
+      const headers = parse(csvString, {
+        skip_empty_lines: true
+      })[0];
+
+      let latField = 'latitude';
+      let lonField = 'longitude';
+      if (headers.includes('緯度')) latField = '緯度';
+      if (headers.includes('経度')) lonField = '経度';
+
+      csv2geojson.csv2geojson(csvString, {
+        latfield: latField,
+        lonfield: lonField,
+        delimiter: ','
+      },
+        function(err, data) {
         if (err) {
           console.error(err);
           throw err;
